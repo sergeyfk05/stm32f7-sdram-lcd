390fb814a4a2bb5d3875a0c40b6fbaadaa959230
sergeyfk05 <sergeyfk05@gmail.com>
2020-08-27 11:22:30 +0300

    mallock is works

--- sdram/sdram.cpp
+++ sdram/sdram.cpp
@@ -2,17 +2,44 @@
 #include <stm32_hal_legacy.h>
 #include <CoreHelper.h>
 #include <SDRAMHelper.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
 
 using namespace Helpers;
 
 #ifdef __cplusplus
 extern "C"
-#endif
-void SysTick_Handler(void)
 {
-	HAL_IncTick();
-	HAL_SYSTICK_IRQHandler();
+#endif
+	void SysTick_Handler(void)
+	{
+		HAL_IncTick();
+		HAL_SYSTICK_IRQHandler();
+	}
+
+
+	extern char _heap_min;
+	extern char _heap_max;
+	extern int errno;
+
+	caddr_t _sbrk(int incr) {
+		static char *current = &_heap_min;
+		char *previous = current;
+
+		if (current + incr > (char*)&_heap_max) {
+			errno = ENOMEM;
+			return (caddr_t) - 1;
+		}
+
+		current += incr;
+		return (caddr_t) previous ;
+	}
+
+#ifdef __cplusplus
 }
+#endif
+
 
 int main(void)
 {
@@ -28,6 +55,6 @@ int main(void)
 	
 	while (1)
 	{
-		status = SDRAMHelper::SDRAMTest();
+		
 	}
 }
